d8d80fe591ab0bdee61ad463380ab7d4d3726153
Jan <31162382+AEon-Jan@users.noreply.github.com>
2026-01-07 12:59:03 +0100

    Update plugin author metadata

--- app/Plugins/ActiveDirectoryAuth/config.php
+++ app/Plugins/ActiveDirectoryAuth/config.php
@@ -3,8 +3,8 @@
 return [
     'name' => 'ActiveDirectoryAuth',
     'description' => 'Active Directory (LDAP) authentication for Faveo login.',
-    'author' => 'Faveo',
-    'website' => 'https://www.faveohelpdesk.com',
+    'author' => 'In-house',
+    'website' => '',
     'version' => '1.0.0',
     'settings' => 'active-directory-auth/settings',
 ];
